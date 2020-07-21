1703ad639d7bd64f79ebf78791c05441216b9190
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-21 11:06:45 +0500

    2.2. Модель данных - Post.[285211#291474]

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,19 +1,31 @@
+import java.util.Date;
+
 public class Post {
     private int id;
     private String subject;
     private String link;
     private String description;
+    private Date create_date;
 
-    public Post(String subject, String link, String description) {
+    public Post(String subject, String link, String description, Date create_date) {
         this.subject = subject;
         this.link = link;
         this.description = description;
+        this.create_date = create_date;
+    }
+
+    public Post(String subject, String link, Date create_date) {
+        this.subject = subject;
+        this.link = link;
+        this.description = "";
+        this.create_date = create_date;
     }
 
     public Post(String subject, String link) {
         this.subject = subject;
         this.link = link;
         this.description = "";
+        this.create_date = new Date();
     }
 
     public int getId() {

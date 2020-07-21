dc000885d660a4dee09bfd0a5aba2511949f9f5a
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-21 10:41:39 +0500

    2.1. Преобразование даты[289476#291477]

--- src/main/java/SqlRuParse.java
+++ src/main/java/SqlRuParse.java
@@ -3,8 +3,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class SqlRuParse {
+
+    private static final String TODAY_TEXT = "сегодня";
+    private static final String YESTERDAY_TEXT = "вчера";
+
+    /**
+     * В полях с датой надо производить замену текста Сегодня и Вчера на соответствующие даты
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        SimpleDateFormat form = new SimpleDateFormat("dd MMM yy, hh:mm" );
+        SimpleDateFormat shortForm = new SimpleDateFormat("dd MMM yy" );
+        Date yesterday = yesterday();
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
@@ -13,7 +30,21 @@ public class SqlRuParse {
             System.out.println(href.text());
             Element parent = td.parent();
             Element dataElement = parent.child(5);
-            System.out.println(dataElement.text());
+            String dataElementText = dataElement.text()
+                    .replace(TODAY_TEXT, shortForm.format(new Date()))
+                    .replace(YESTERDAY_TEXT, shortForm.format(yesterday));
+            Date oldD =  form.parse(dataElementText);
+            System.out.println(oldD);
         }
+    }
+
+    /**
+     * все украдено до нас https://fooobar.com/questions/64063/get-yesterdays-date-using-date
+     * @return
+     */
+    private static Date yesterday() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        return cal.getTime();
     }
 }
